35b48494bb6548c3cdc7ad731e544ff6c6201824
kennyweigel <kennyweigel@gmail.com>
2015-03-31 21:49:27 -0400

    customized admin appearance of questions

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from polls.models import Question
 
 # Register your models here.
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
